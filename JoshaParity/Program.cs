642a01945d883a9205512c6c6854b48141a6c091
Joshabi <joshuawright712@hotmail.co.uk>
2023-01-20 21:14:48 +0000

    Project Setup

---
+++ JoshaParity/Program.cs
@@ -0,0 +1,18 @@
+﻿using JoshaUtils;
+
+Console.WriteLine("<< Josha Parity Check! >>");
+
+List<Beatmap?> maps = new();
+string mapFolder = "./Maps";
+maps.Add(BeatmapLoader.LoadMap($"{mapFolder}/Voracity/"));
+
+foreach(Beatmap? map in maps)
+{
+    if (map != null)
+    {
+        Console.WriteLine($"Map Name: {map._songName} {map._songSubName}by {map._songAuthorName}" +
+            $"\nMapped by: {map._levelAuthorName}" +
+            $"\nBPM of: {map._beatsPerMinute}" +
+            $"\nTotal of {map.Difficulties.Count} difficulties.");
+    }
+}
