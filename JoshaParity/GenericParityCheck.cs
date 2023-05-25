090a033e7e40cc4772cb19fc9a8697dc5e7f3791
Joshabi <joshuawright712@hotmail.co.uk>
2023-05-25 01:24:12 +0100

    Refactor Map Loading
    Refactored map loading to support V3 and convert V2 to V3 when checking, big thank you to Jindo for allowing me to backport some of it from JindoRankTool.
    
    NEXT STEP:
    - Implement Chain detection and logic into parity checker so V3 is fully supported
    - Create necessary functions to convert  the checker into a library for use in other mods and programs

--- JoshaParity/GenericParityCheck.cs
+++ JoshaParity/GenericParityCheck.cs
@@ -10,32 +10,32 @@ namespace JoshaParity
 
         // Returns true if the inputted note and bomb coordinates cause a reset potentially
         private readonly Dictionary<int, Func<Vector2, int, int, PARITY_STATE, bool>> _bombDetectionConditions = new()
-    {
-        { 0, (note, x, y, parity) => ((y >= note.Y && y != 0) || (y > note.Y && y > 0)) && x == note.X },
-        { 1, (note, x, y, parity) => ((y <= note.Y && y != 2) || (y < note.Y && y < 2)) && x == note.X },
-        { 2, (note, x, y, parity) => (parity == PARITY_STATE.FOREHAND && (y == note.Y || y == note.Y - 1) && ((note.X != 3 && x < note.X) || (note.X < 3 && x <= note.X))) ||
-            (parity == PARITY_STATE.BACKHAND && y == note.Y && ((note.X != 0 && x < note.X) || (note.X > 0 && x <= note.X))) },
-        { 3, (note, x, y, parity) => (parity == PARITY_STATE.FOREHAND && (y == note.Y || y == note.Y - 1) && ((note.X != 0 && x > note.X) || (note.X > 0 && x >= note.X))) ||
-            (parity == PARITY_STATE.BACKHAND && y == note.Y && ((note.X != 3 && x > note.X) || (note.X < 3 && x >= note.X))) },
-        { 4, (note, x, y, parity) => ((y >= note.Y && y != 0) || (y > note.Y && y > 0)) && x == note.X && x != 3 && parity != PARITY_STATE.FOREHAND },
-        { 5, (note, x, y, parity) => ((y >= note.Y && y != 0) || (y > note.Y && y > 0)) && x == note.X && x != 0 && parity != PARITY_STATE.FOREHAND },
-        { 6, (note, x, y, parity) => ((y <= note.Y && y != 2) || (y < note.Y && y < 2)) && x == note.X && x != 3 && parity != PARITY_STATE.BACKHAND },
-        { 7, (note, x, y, parity) => ((y <= note.Y && y != 2) || (y < note.Y && y < 2)) && x == note.X && x != 0 && parity != PARITY_STATE.BACKHAND },
-        { 8, (note,x,y, parity) => false }
-    };
+        {
+            { 0, (note, x, y, parity) => ((y >= note.Y && y != 0) || (y > note.Y && y > 0)) && x == note.X },
+            { 1, (note, x, y, parity) => ((y <= note.Y && y != 2) || (y < note.Y && y < 2)) && x == note.X },
+            { 2, (note, x, y, parity) => (parity == PARITY_STATE.FOREHAND && (y == note.Y || y == note.Y - 1) && ((note.X != 3 && x < note.X) || (note.X < 3 && x <= note.X))) ||
+                (parity == PARITY_STATE.BACKHAND && y == note.Y && ((note.X != 0 && x < note.X) || (note.X > 0 && x <= note.X))) },
+            { 3, (note, x, y, parity) => (parity == PARITY_STATE.FOREHAND && (y == note.Y || y == note.Y - 1) && ((note.X != 0 && x > note.X) || (note.X > 0 && x >= note.X))) ||
+                (parity == PARITY_STATE.BACKHAND && y == note.Y && ((note.X != 3 && x > note.X) || (note.X < 3 && x >= note.X))) },
+            { 4, (note, x, y, parity) => ((y >= note.Y && y != 0) || (y > note.Y && y > 0)) && x == note.X && x != 3 && parity != PARITY_STATE.FOREHAND },
+            { 5, (note, x, y, parity) => ((y >= note.Y && y != 0) || (y > note.Y && y > 0)) && x == note.X && x != 0 && parity != PARITY_STATE.FOREHAND },
+            { 6, (note, x, y, parity) => ((y <= note.Y && y != 2) || (y < note.Y && y < 2)) && x == note.X && x != 3 && parity != PARITY_STATE.BACKHAND },
+            { 7, (note, x, y, parity) => ((y <= note.Y && y != 2) || (y < note.Y && y < 2)) && x == note.X && x != 0 && parity != PARITY_STATE.BACKHAND },
+            { 8, (note,x,y, parity) => false }
+        };
 
-        public bool BombResetCheck(SwingData lastCut, List<Note> bombs, int xPlayerOffset)
+        public bool BombResetCheck(SwingData lastCut, List<Bomb> bombs, int xPlayerOffset)
         {
             // Not found yet
             bool bombResetIndicated = false;
             for (int i = 0; i < bombs.Count; i++)
             {
                 // Get current bomb
-                Note bomb = bombs[i];
-                Note? note;
+                Bomb bomb = bombs[i];
+                Note note;
 
                 // If in the center 2 grid spaces, no point trying
-                if ((bomb._lineIndex == 1 || bomb._lineIndex == 2) && bomb._lineLayer == 1) continue;
+                if ((bomb.x == 1 || bomb.x == 2) && bomb.y == 1) continue;
 
                 // Get the last note. In the case of a stack, picks the note that isnt at 2 or 0 as
                 // it triggers a reset when it shouldn't.
@@ -49,21 +49,21 @@ namespace JoshaParity
                 // Offset the checking if the entire outerlane bombs indicate moving the check inwards
                 int xOffset = 0;
 
-                bool bombOffsetting = bombs.Any(bomb => bomb._lineIndex == note._lineIndex && (bomb._lineLayer <= note._lineLayer && lastCut.swingParity == PARITY_STATE.BACKHAND && lastCut.endPos.rotation >= 0)) ||
-                    bombs.Any(bomb => bomb._lineIndex == note._lineIndex && (bomb._lineLayer >= note._lineLayer && lastCut.swingParity == PARITY_STATE.FOREHAND && lastCut.endPos.rotation >= 0));
+                bool bombOffsetting = bombs.Any(bomb => bomb.x == note.x && (bomb.y <= note.y && lastCut.swingParity == PARITY_STATE.BACKHAND && lastCut.endPos.rotation >= 0)) ||
+                    bombs.Any(bomb => bomb.x == note.x && (bomb.y >= note.y && lastCut.swingParity == PARITY_STATE.FOREHAND && lastCut.endPos.rotation >= 0));
 
-                if (bombOffsetting && note._lineIndex == 0) xOffset = 1;
-                if (bombOffsetting && note._lineIndex == 3) xOffset = -1;
+                if (bombOffsetting && note.x == 0) xOffset = 1;
+                if (bombOffsetting && note.x == 3) xOffset = -1;
 
                 // Determine if lastnote and current bomb cause issue
                 // If we already found reason to reset, no need to try again
-                bombResetIndicated = _bombDetectionConditions[lastNoteCutDir](new Vector2(note._lineIndex, note._lineLayer), bomb._lineIndex - (xPlayerOffset * 2) - xOffset, bomb._lineLayer, lastCut.swingParity);
+                bombResetIndicated = _bombDetectionConditions[lastNoteCutDir](new Vector2(note.x, note.y), bomb.x - (xPlayerOffset * 2) - xOffset, bomb.y, lastCut.swingParity);
                 if (bombResetIndicated) return true;
             }
             return false;
         }
 
-        public PARITY_STATE ParityCheck(SwingData lastCut, ref SwingData currentSwing, List<Note> bombs, int playerXOffset, bool rightHand, float timeTillNextNote = 0.1f)
+        public PARITY_STATE ParityCheck(SwingData lastCut, ref SwingData currentSwing, List<Bomb> bombs, int playerXOffset, bool rightHand, float timeTillNextNote = 0.1f)
         {
             // AFN: Angle from neutral
             // Assuming a forehand down hit is neutral, and a backhand up hit
@@ -74,11 +74,11 @@ namespace JoshaParity
             Note nextNote = currentSwing.notes[0];
 
             float currentAFN = (lastCut.swingParity != PARITY_STATE.FOREHAND) ?
-                SwingDataGeneration.BackhandDict[lastCut.notes[0]._cutDirection] :
-                SwingDataGeneration.ForehandDict[lastCut.notes[0]._cutDirection];
+                SwingDataGeneration.BackhandDict[lastCut.notes[0].d] :
+                SwingDataGeneration.ForehandDict[lastCut.notes[0].d];
 
-            int orient = nextNote._cutDirection;
-            if (nextNote._cutDirection == 8) orient = (lastCut.swingParity == PARITY_STATE.FOREHAND) ?
+            int orient = nextNote.d;
+            if (nextNote.d == 8) orient = (lastCut.swingParity == PARITY_STATE.FOREHAND) ?
                     SwingDataGeneration.BackhandDict.FirstOrDefault(x => x.Value == Math.Round(lastCut.endPos.rotation / 45.0) * 45).Key :
                     SwingDataGeneration.ForehandDict.FirstOrDefault(x => x.Value == Math.Round(lastCut.endPos.rotation / 45.0) * 45).Key;
 
@@ -91,11 +91,11 @@ namespace JoshaParity
             _upsideDown = false;
 
             // Determines if potentially an upside down hit based on note cut direction and last swing angle
-            if (lastCut.swingParity == PARITY_STATE.BACKHAND && lastCut.endPos.rotation > 0 && (nextNote._cutDirection == 0 || nextNote._cutDirection == 8))
+            if (lastCut.swingParity == PARITY_STATE.BACKHAND && lastCut.endPos.rotation > 0 && (nextNote.d == 0 || nextNote.d == 8))
             {
                 _upsideDown = true;
             }
-            else if (lastCut.swingParity == PARITY_STATE.FOREHAND && lastCut.endPos.rotation > 0 && (nextNote._cutDirection == 1 || nextNote._cutDirection == 8))
+            else if (lastCut.swingParity == PARITY_STATE.FOREHAND && lastCut.endPos.rotation > 0 && (nextNote.d == 1 || nextNote.d == 8))
             {
                 _upsideDown = true;
             }
@@ -109,7 +109,7 @@ namespace JoshaParity
             bool bombResetParityImplied = false;
             if (bombResetIndicated)
             {
-                if (nextNote._cutDirection == 8 && lastCut.notes.All(x => x._cutDirection == 8)) bombResetParityImplied = true;
+                if (nextNote.d == 8 && lastCut.notes.All(x => x.d == 8)) bombResetParityImplied = true;
                 else
                 {
                     // In case of dots, calculate using previous swing swing-angle
@@ -119,11 +119,11 @@ namespace JoshaParity
 
                     if (lastCut.swingParity == PARITY_STATE.FOREHAND)
                     {
-                        if (Math.Abs(SwingDataGeneration.ForehandDict[altOrient] + SwingDataGeneration.BackhandDict[nextNote._cutDirection]) >= 90) { bombResetParityImplied = true; }
+                        if (Math.Abs(SwingDataGeneration.ForehandDict[altOrient] + SwingDataGeneration.BackhandDict[nextNote.d]) >= 90) { bombResetParityImplied = true; }
                     }
                     else
                     {
-                        if (Math.Abs(SwingDataGeneration.BackhandDict[altOrient] + SwingDataGeneration.ForehandDict[nextNote._cutDirection]) >= 90) { bombResetParityImplied = true; }
+                        if (Math.Abs(SwingDataGeneration.BackhandDict[altOrient] + SwingDataGeneration.ForehandDict[nextNote.d]) >= 90) { bombResetParityImplied = true; }
                     }
                 }
             }
@@ -136,7 +136,7 @@ namespace JoshaParity
             }
 
             // If last cut is entirely dot notes and next cut is too, then parity is assumed to be maintained
-            if (lastCut.notes.All(x => x._cutDirection == 8) && currentSwing.notes.All(x => x._cutDirection == 8))
+            if (lastCut.notes.All(x => x.d == 8) && currentSwing.notes.All(x => x.d == 8))
             {
                 return (lastCut.swingParity == PARITY_STATE.FOREHAND) ? PARITY_STATE.BACKHAND : PARITY_STATE.FOREHAND;
             }
